b92838c1e483598bde434ffc1ce053209ebf1c1d
Omer-zil <omerabubeker0851@gmail.com>
2026-05-07 16:00:37 +0300

    Added maze solver using backtracking

--- maze_GUI.py
+++ maze_GUI.py
@@ -24,6 +24,7 @@ canvas.pack()
 northWall = [[1 for _ in range(COLS)] for _ in range(ROWS + 1)]
 eastWall = [[1 for _ in range(COLS + 1)] for _ in range(ROWS)]
 visited = [[False for _ in range(COLS)] for _ in range(ROWS)]
+solverVisited = [[False for _ in range(COLS)] for _ in range(ROWS)]
 
 
 # VALID CELL
@@ -141,6 +142,97 @@ def generate_maze():
 
         else:
             stack.pop()
+
+  
+  # CHECK MOVEMENT
+def can_move(r1, c1, r2, c2):
+
+    if not valid(r2, c2):
+        return False
+
+    dr = r2 - r1
+    dc = c2 - c1
+
+    if dr == -1:
+        return northWall[r1][c1] == 0
+    elif dr == 1:
+        return northWall[r2][c2] == 0
+    elif dc == 1:
+        return eastWall[r1][c1 + 1] == 0
+    elif dc == -1:
+        return eastWall[r1][c1] == 0
+
+    return False
+
+# COLOR CELL
+def color_cell(r, c, color):
+
+    padding = 6
+
+    x1 = c * CELL_SIZE + padding
+    y1 = r * CELL_SIZE + padding
+
+    x2 = x1 + CELL_SIZE - 2 * padding
+    y2 = y1 + CELL_SIZE - 2 * padding
+
+    canvas.create_oval(
+        x1,
+        y1,
+        x2,
+        y2,
+        fill=color,
+        outline=color
+    )
+
+    root.update()
+    time.sleep(DELAY)
+
+
+# SOLVE MAZE
+# BACKTRACKING
+
+def solve_maze(r, c, end_r, end_c):
+
+    solverVisited[r][c] = True
+
+    # current mouse
+    color_cell(r, c, "red")
+
+    if r == end_r and c == end_c:
+
+        # correct path
+        color_cell(r, c, "green")
+        return True
+
+    directions = [
+        (-1, 0),
+        (1, 0),
+        (0, -1),
+        (0, 1)
+    ]
+
+    random.shuffle(directions)
+
+    for dr, dc in directions:
+
+        nr = r + dr
+        nc = c + dc
+
+        if (
+            valid(nr, nc)
+            and not solverVisited[nr][nc]
+            and can_move(r, c, nr, nc)
+        ):
+
+            if solve_maze(nr, nc, end_r, end_c):
+
+                color_cell(r, c, "green")
+                return True
+
+    # dead end
+    color_cell(r, c, "blue")
+
+    return False
 
 
 # START PROGRAM
